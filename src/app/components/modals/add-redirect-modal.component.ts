85289b5e8af315dc69b306bebd2788b261f96a1a
Гвоздев Михаил Васильевич <mgvozdev@users.vgtrk>
2018-11-09 17:20:28 +0300

    fix redirect

--- src/app/components/modals/add-redirect-modal.component.ts
+++ src/app/components/modals/add-redirect-modal.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
-import { Subscription, forkJoin } from 'rxjs';
-import { finalize, map } from 'rxjs/operators';
+import { finalize, debounceTime, distinctUntilChanged, takeWhile, first, filter, startWith } from 'rxjs/operators';
 
 import { AuthenticationService } from '../../services/auth.service';
 import { SearchService } from '../../services/search.service';
@@ -13,16 +13,20 @@ import { AlertService } from '../../services/alert.service';
     selector: 'add-redirect-modal',
     templateUrl: './add-redirect-modal.component.html'
 })
-export class AddRedirectModal {
+export class AddRedirectModal implements OnInit, OnDestroy{
 
-    error: any = null;
+    urlForm: FormGroup;
+
+    //error: any = null;
     itemRedirect: any = null;
     public loading: boolean = false;
+    private alive: boolean = true;
 
     @Input() pathId: number;
     @Output() onSetRedirect = new EventEmitter();
 
     constructor(
+        private formBuilder: FormBuilder,
         private activeModal: NgbActiveModal,
         private searchService: SearchService,
         private pathService: PathService,
@@ -30,11 +34,31 @@ export class AddRedirectModal {
         private authenticationService: AuthenticationService,
     ) {}
 
+    ngOnInit() {
+        this.urlForm = this.formBuilder.group({
+          url: [' https://www.newstube.ru/obrashchenie-putina', Validators.required],
+        });
+
+        this.urlForm.get('url').valueChanges
+            .pipe( 
+                takeWhile(() => this.alive),             
+                debounceTime(300),
+                distinctUntilChanged(),         
+            )
+            .subscribe( val => {
+                this.getUrl(val);
+        });
+    }
+
+    ngOnDestroy() { 
+        this.alive = false;
+    }
+
 
     public showObject(obj: any | boolean) {
         if (obj) {   
             this.itemRedirect = obj;
-            this.error = null;
+            //this.error = null;
             /*
             if(confirm(
                 `Вы уверены что хотите установить Redirect на объект "${obj.ObjectTypeName}: ${obj.Name}"?`
@@ -45,13 +69,13 @@ export class AddRedirectModal {
         }
         else {
             this.itemRedirect = null;
-            this.error = "Путь не найден";
+            //this.error = "Путь не найден";
         }
     }
 
-    public setRedirect(obj: any) {
+    public setRedirect(url: any) {
         this.loading = true;
-        this.pathService.createRedirect(this.authenticationService.sessionId, this.pathId, {Url: obj.Url})
+        this.pathService.createRedirect(this.authenticationService.sessionId, this.pathId, {Url: url})
         .pipe( finalize( () => this.loading = false ) )
         .subscribe(
             data => {
@@ -60,5 +84,26 @@ export class AddRedirectModal {
                 this.onSetRedirect.emit(true);
             }
         );
-	}
+    }
+
+    public getUrl(url) {
+        this.loading = true;
+        this.itemRedirect = null;
+        this.pathService.getByUrl(this.authenticationService.sessionId, url)
+        .pipe( finalize(() => this.loading = false))
+        .subscribe(data => this.itemRedirect = data);
+    }
+
+    onSubmit() {    
+        if (this.urlForm.invalid) {
+            return;
+        }
+
+        if(confirm(
+            `Вы уверены что хотите установить Redirect на Url "${this.urlForm.controls.url.value}"?`
+        )) {
+            this.setRedirect(this.urlForm.controls.url.value);
+        }
+
+    }
 }
